74fbacef8dd286bb7ac4cb38c4eec70f5d5d5197
AkumuOmolo <akumu.omoloh@gmail.com>
2025-08-23 21:01:46 +0300

    Adds articles code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,154 @@
 class Article:
+
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        # only allow setting once
+        if hasattr(self, "_title"):
+            return
+
+        if not isinstance(value, str):
+            return
+
+        if not (5 <= len(value) <= 50):
+            return
+
+        self._title = value
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        from classes.many_to_many import Author
+        if not isinstance(value, Author):
+            return
+        self._author = value
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, value):
+        from classes.many_to_many import Magazine
+        if not isinstance(value, Magazine):
+            return
+        self._magazine = value
+
+
 class Author:
+
+    all = []
+
     def __init__(self, name):
         self.name = name
+        Author.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+
+        if hasattr(self, "_name"):
+            return
+
+        if not isinstance(value, str):
+            return
+
+        if len(value) == 0:
+            return
+
+        self._name = value
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list({article.magazine for article in self.articles()})
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        #     mags = self.magazines()
+        #    if not mags:
+        #          return None
+        areas = list({magazine.category for magazine in self.magazines()})
+        if not areas:
+            return None
+
+        return areas
+
 
 class Magazine:
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string.")
+        if not (2 <= len(value) <= 16):
+            raise ValueError("Name must be between 2 and 16 characters.")
+
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string.")
+
+        if len(value) == 0:
+            raise TypeError("Categories must be longer than 0 characters.")
+        self._category = value
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in Article.all if article.magazine == self]))
 
     def article_titles(self):
-        pass
+        titles = [
+            article.title for article in Article.all if article.magazine == self]
+
+        return titles if titles else None
+
 
     def contributing_authors(self):
-        pass
+        authors_count = {}
+
+        for article in self.articles():
+            author = article.author
+            authors_count[author] = authors_count.get(author, 0) + 1
+
+        result = [author for author, count in authors_count.items() if count > 2]
+
+        return result if result else None
